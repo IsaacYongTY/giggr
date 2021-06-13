ff40d493f659c00c1a4aa317d0502efebde5fa13
IsaacYongTY <isaac.yongty@gmail.com>
2021-06-14 02:18:35 +0800

    fix type bug for Musician

--- pages/test/playground.tsx
+++ pages/test/playground.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from '../../components/layouts/Layout';
+import MusiciansDropdown from "../../components/MusiciansDropdown";
 
 export default function Playground() {
     
     return (
         <Layout title="Playground">
             <div>something here</div>
+
+            <MusiciansDropdown />
         </Layout>
     )
 }
