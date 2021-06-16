a5adbb161018d733119e72e534150db973e0ebd8
IsaacYongTY <isaac.yongty@gmail.com>
2021-06-16 22:28:51 +0800

    completed KeysDropDown, fix UI bugs

---
+++ lib/utils/convert-key-to-key-mode-int.ts
@@ -0,0 +1,34 @@
+
+
+import { noteArray } from "../data/data"
+
+export default function convertKeyToKeyModeInt(keyString : string) : number[] {
+
+    let note;
+    let mode = 1;
+
+    if (keyString[keyString.length - 1] === 'm') {
+        mode = 0
+        note = keyString.slice(0, keyString.length - 1)
+    } else {
+        note = keyString
+    }
+
+    //enharmonic
+    if(note === 'C#') {
+        note = 'Db'
+    }
+
+    if(note === 'F#') {
+        note = 'Gb'
+    }
+
+    if(note === 'G#') {
+        note = 'Ab'
+    }
+
+    if(!noteArray.includes(note)) return [-1,-1]
+
+    return [noteArray.indexOf(note), mode]
+}
+
