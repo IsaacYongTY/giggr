72af525f99cacf63160687617c5f3b46263b82cd
IsaacYongTY <isaac.yongty@gmail.com>
2021-06-20 03:31:03 +0800

    add testing to KeysDropdown

---
+++ __tests__/AddSongModal.test..js
@@ -0,0 +1,144 @@
+import KeysDropdown from "../components/KeysDropdown";
+import React from "react";
+import { screen, render, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import AddSongModal from "../components/elements/AddSongModal";
+import "@testing-library/jest-dom"
+import '@testing-library/jest-dom/extend-expect'
+import axios from 'axios'
+
+jest.mock('axios')
+
+function renderAddSongModal(props) {
+    const utils = render(
+        <AddSongModal
+            isModalOpen={true}
+            setIsModalOpen={jest.fn()}
+            type="add"
+            database="database1"
+            setSongs={jest.fn()}
+            musicians={[{name: "test1"}, {name: "test2"}]}
+            setMusicians={jest.fn()}
+
+            {...props}
+        />
+    )
+    const isMinorCheckbox = utils.getByLabelText(/minor/i)
+    const keysDropdown = utils.getByLabelText(/key/i)
+    return {...utils, isMinorCheckbox, keysDropdown}
+}
+
+describe("<AddSongModal />", () => {
+    it("should render the component", () => {
+        const setIsModalOpen = jest.fn()
+        const setSongs = jest.fn()
+        const setMusicians = jest.fn()
+        const getLanguages = jest.fn()
+        getLanguages.mockResolvedValue({
+            response: {
+                data: {
+                    languages: []
+                }
+            }
+        })
+        render(<AddSongModal isModalOpen={true} setIsModalOpen={setIsModalOpen} type="add" database="database1"
+                             setSongs={setSongs} musicians={[{name: "test1"}, {name: "test2"}]}
+                             setMusicians={setMusicians}/>)
+
+        expect(screen.getByLabelText(/key/i)).toBeInTheDocument()
+        expect(screen.getByLabelText(/artist/i)).toBeInTheDocument()
+    })
+
+})
+
+describe("KeysDropdown component's behaviours", () => {
+    it("should render the component", () => {
+        const { keysDropdown, isMinorCheckbox } = renderAddSongModal()
+
+        expect(keysDropdown).toBeInTheDocument()
+        expect(isMinorCheckbox).toBeInTheDocument()
+
+    })
+
+
+    it("should render the component with song's key if exist and in Edit mode", async () => {
+
+        renderAddSongModal({
+            song: {key: 0, mode: 1},
+            type: "edit"
+        })
+
+
+        screen.debug(undefined, 12000)
+        expect(await screen.findByText("C")).toBeInTheDocument()
+
+
+        cleanup()
+
+        renderAddSongModal({
+            type: "edit",
+            song: {key: 11, mode: 0}
+        })
+
+        expect(screen.getByDisplayValue("Bm")).toBeInTheDocument()
+
+    })
+
+    it("should toggle the dropdown menu and render key options accordingly", () => {
+        const { keysDropdown } = renderAddSongModal()
+
+        expect(keysDropdown).toBeInTheDocument()
+
+        userEvent.click(keysDropdown)
+
+        // use arrow down to simulate opening the dropdown menu via clicking
+        userEvent.type(keysDropdown,'{arrowdown}')
+        userEvent.click(screen.getByText('C'))
+
+        expect(screen.getByDisplayValue('C')).toBeInTheDocument()
+
+        userEvent.type(keysDropdown,'{arrowdown}')
+        // screen.debug(undefined, 10000)
+        userEvent.click(screen.getByText('Eb'))
+        expect(screen.getByDisplayValue('Eb')).toBeInTheDocument()
+
+    })
+
+    it("should toggle the isMinor checkbox", () => {
+        let { keysDropdown, isMinorCheckbox } = renderAddSongModal()
+
+        userEvent.click(keysDropdown)
+        expect(screen.getByText('C')).toBeInTheDocument()
+        expect(screen.getByText('Bb')).toBeInTheDocument()
+
+        userEvent.click(isMinorCheckbox)
+        expect(isMinorCheckbox).toBeChecked()
+
+        userEvent.click(keysDropdown)
+        expect(screen.getByText('Cm')).toBeInTheDocument()
+        expect(screen.getByText('Gm')).toBeInTheDocument()
+        expect(screen.getByText('Am')).toBeInTheDocument()
+        //use query only if the element cannot be found
+        expect(screen.queryByText('C')).not.toBeInTheDocument()
+        expect(screen.queryByText('G')).not.toBeInTheDocument()
+
+        userEvent.click(isMinorCheckbox)
+        userEvent.click(keysDropdown)
+
+        expect(isMinorCheckbox).not.toBeChecked()
+        expect(screen.getByText('A')).toBeInTheDocument()
+        expect(screen.getByText('D')).toBeInTheDocument()
+        expect(screen.queryByText('Cm')).not.toBeInTheDocument()
+        expect(screen.queryByText('Gm')).not.toBeInTheDocument()
+    })
+})
+
+
+
+
+
+
+
+
+
+// })
---
+++ __tests__/convert-key-to-key-mode-int.test.js
@@ -0,0 +1,36 @@
+import convertKeyToKeyModeInt from "../lib/utils/convert-key-to-key-mode-int";
+
+describe("convertKeyToKeyModeInt", () => {
+    it("should convert key to [key, mode]", () => {
+        expect(convertKeyToKeyModeInt("C")).toStrictEqual([0,1])
+        expect(convertKeyToKeyModeInt("Cm")).toStrictEqual([0,0])
+        expect(convertKeyToKeyModeInt("Bm")).toStrictEqual([11,0])
+        expect(convertKeyToKeyModeInt("A")).toStrictEqual([9,1])
+    })
+
+    it("should convert key to [key,mode] with correct enharmonic equivalent", () => {
+        expect(convertKeyToKeyModeInt("C#m")).toStrictEqual([1,0])
+        expect(convertKeyToKeyModeInt("G#m")).toStrictEqual([8,0])
+        expect(convertKeyToKeyModeInt("F#m")).toStrictEqual([6,0])
+    })
+
+    it("should convert key to [key,mode] with correct enharmonic equivalent with wrong input enharmonic", () => {
+        expect(convertKeyToKeyModeInt("Dbm")).toStrictEqual([1,0])
+        expect(convertKeyToKeyModeInt("Abm")).toStrictEqual([8,0])
+        expect(convertKeyToKeyModeInt("Gbm")).toStrictEqual([6,0])
+    })
+
+    it("should return [-1,-1] if the input is invalid",() => {
+        expect(convertKeyToKeyModeInt("Hm")).toStrictEqual([-1,-1])
+        expect(convertKeyToKeyModeInt("123")).toStrictEqual([-1,-1])
+        expect(convertKeyToKeyModeInt("AM")).toStrictEqual([-1,-1])
+        expect(convertKeyToKeyModeInt("1m")).toStrictEqual([-1,-1])
+        expect(convertKeyToKeyModeInt("1")).toStrictEqual([-1,-1])
+    })
+
+    it("should return [-1,-1] if the input is undefined", () => {
+        expect(convertKeyToKeyModeInt("")).toStrictEqual([-1,-1])
+        expect(convertKeyToKeyModeInt(undefined)).toStrictEqual([-1,-1])
+        expect(convertKeyToKeyModeInt(null)).toStrictEqual([-1,-1])
+    })
+})
