522d7831ed13d35949adef640de6df3be3ede6ce
Isaac Yong <isaac.yongty@gmail.com>
2021-06-27 16:58:00 +0800

    working on showing single user repertoire

--- __tests__/AddSongModal.test.js
+++ __tests__/AddSongModal.test.js
@@ -5,7 +5,7 @@ import AddSongModal from "../components/elements/AddSongModal";
 import "@testing-library/jest-dom"
 import '@testing-library/jest-dom/extend-expect'
 import axios from 'axios'
-import { loadUserRepertoire, loadMusicians, loadLanguages } from "../lib/library";
+import { loadUserRepertoire, loadUserMusicians, loadUserLanguages } from "../lib/library";
 
 jest.mock('axios')
 // jest.mock('../lib/library')
