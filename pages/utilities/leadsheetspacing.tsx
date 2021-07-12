ee2cfc0f5d04be00ca25d4cfec4c5f0efc910f31
IsaacYongTY <isaac.yongty@gmail.com>
2021-07-12 13:23:40 +0800

    working on Lead Sheet Spacing page

--- pages/utilities/leadsheetspacing.tsx
+++ pages/utilities/leadsheetspacing.tsx
@@ -1,10 +1,38 @@
-import React from "react"
+import React, {useState} from "react"
+import Layout from "../../components/layouts/Layout";
+import addSpaceBetweenChineseWords from "../../lib/utils/add-space-between-chinese-words";
+
+
 
 export default function LeadSheetSpacing() {
 
+    const [inputText, setInputText] = useState("")
+    const [resultText, setResultText] = useState("")
+
+    function handleProcessText() {
+        const result = addSpaceBetweenChineseWords(inputText)
+        setResultText(result)
+    }
+
+    function handleClearAll() {
+        setInputText("")
+        setResultText("")
+    }
     return (
-        <div>
+        <Layout user={{ id: 1, isAdmin: false }}>
             Lead Sheet Spacing
-        </div>
+            <label>
+                <div>Input:</div>
+                <textarea value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+            </label>
+
+            <label>
+                <div>Result:</div>
+                <textarea value={resultText} onChange={(e) => setResultText(e.target.value)}/>
+            </label>
+
+            <button onClick={handleProcessText}>Process</button>
+            <button onClick={handleClearAll}>Clear All</button>
+        </Layout>
     )
 }
