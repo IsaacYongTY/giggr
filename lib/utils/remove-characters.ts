b8f4a3967a2bf0a765343ac0136eb9cfe04c18c6
IsaacYongTY <isaac.yongty@gmail.com>
2021-07-12 20:08:55 +0800

    added new functions and tests for lead sheet spacing

---
+++ lib/utils/remove-characters.ts
@@ -0,0 +1,20 @@
+
+
+export function isEscapedCharacter(char: string) {
+    let regexEscapedCharacters = "[\\^$.|?*+()"
+    return regexEscapedCharacters.split("").indexOf(char) > -1
+}
+
+export function addBackSlashToEscapingCharactersArray(strArray: string[]) : string[] {
+    return strArray.map(char => isEscapedCharacter(char) ? `\\${char}` : char)
+}
+
+export default function removeCharacters(stringsToRemoveArray: string[], text: string) : string {
+    if(stringsToRemoveArray.length === 0) return text
+
+    const regexString = addBackSlashToEscapingCharactersArray(stringsToRemoveArray)
+
+    let stringsToRemoveRegex = new RegExp(regexString.join("|"), "g")
+    return text.replace(stringsToRemoveRegex, "")
+
+}
--- pages/utilities/leadsheetspacing.tsx
+++ pages/utilities/leadsheetspacing.tsx
@@ -2,15 +2,33 @@ import React, {useState} from "react"
 import Layout from "../../components/layouts/Layout";
 import addSpaceBetweenChineseWords from "../../lib/utils/add-space-between-chinese-words";
 import styles from "../../assets/scss/pages/_lead-sheet-spacing.module.scss";
+import {hyphenateSync} from "hyphen/en-gb";
+
 
 export default function LeadSheetSpacing() {
 
     const [inputText, setInputText] = useState("")
     const [resultText, setResultText] = useState("")
+    const [isAddHyphen, setIsAddHyphen] = useState(true)
 
-    function handleProcessText() {
-        const result = addSpaceBetweenChineseWords(inputText)
-        setResultText(result)
+    function toggleAddHyphen() {
+        setIsAddHyphen(prevState => !prevState)
+    }
+
+    async function handleProcessText() {
+
+        try {
+            let result = addSpaceBetweenChineseWords(inputText)
+
+            if(isAddHyphen) {
+                result = hyphenateSync(result, { hyphenChar: "-"})
+            }
+
+            setResultText(result)
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 
     function handleClearAll() {
@@ -26,6 +44,7 @@ export default function LeadSheetSpacing() {
         <Layout user={{ id: 1, isAdmin: false }} title="Lead Sheet Spacing">
             <div className={styles.container}>
                 <div className={styles.textAreaContainer}>
+
                     <div>
                         <label>
                             <div>Input:</div>
@@ -36,6 +55,11 @@ export default function LeadSheetSpacing() {
                             <button className="btn btn-primary" onClick={handleProcessText}>Process</button>
                             <button className="btn btn-danger-outlined" onClick={handleClearAll}>Clear All</button>
                         </div>
+
+                        <label>
+                            <input type="checkbox" checked={isAddHyphen} onChange={toggleAddHyphen}/>
+                            <span>Add Hyphen between Syllables (testing)</span>
+                        </label>
                     </div>
 
                     <div>
