5e1bb4143782e4971295c55e3824545ca8c699b6
Isaac Yong <isaac.yongty@gmail.com>
2021-07-24 03:07:10 +0800

    dropdown now shows only musicians with selected role

--- __tests__/pages/repertoire.test.tsx
+++ __tests__/pages/repertoire.test.tsx
@@ -52,179 +52,173 @@ function renderRepertoirePage(props =  {}) {
 }
 describe("The Repertoire Page", () => {
 
-    // describe("The CSV Upload function", () => {
-    //
-    //     it("should open and close the Upload CSV modal", () => {
-    //         const { uploadCsvButton } = renderRepertoirePage()
-    //         userEvent.click(uploadCsvButton)
-    //         expect(screen.getByRole("button", { name: /submit/i})).toBeInTheDocument()
-    //         expect(screen.getByText( /click to upload csv/i)).toBeInTheDocument()
-    //
-    //         const closeIcon = screen.getByText( /close/i)
-    //         userEvent.click(closeIcon)
-    //         expect(screen.queryByRole("button", { name: /submit/i})).not.toBeInTheDocument()
-    //         expect(screen.queryByText( /click to upload csv/i)).not.toBeInTheDocument()
-    //
-    //     })
-    //
-    //     describe("The behaviour of the Upload CSV Modal", () => {
-    //         const errorMessage = /.*please select a \.csv file before submitting.*/i
-    //         const successMessage = /csv uploaded successfully.*/i
-    //
-    //         interface IBlob extends Blob {
-    //             lastModifiedDate?: string,
-    //             name?: string
-    //         }
-    //
-    //         it("should show error message if no file is selected", () => {
-    //             const { uploadCsvButton } = renderRepertoirePage()
-    //
-    //
-    //             userEvent.click(uploadCsvButton)
-    //             const submitButton = screen.getByRole("button", { name: /submit/i})
-    //             const closeIcon = screen.getByText( /close/i)
-    //
-    //             expect(screen.queryByText(errorMessage)).not.toBeInTheDocument()
-    //
-    //             userEvent.click(submitButton)
-    //             expect(screen.getByText(errorMessage)).toBeInTheDocument()
-    //
-    //             userEvent.click(closeIcon)
-    //             userEvent.click(uploadCsvButton)
-    //
-    //             expect(screen.queryByText(errorMessage)).not.toBeInTheDocument()
-    //
-    //         })
-    //
-    //         it("should show the filename if file is selected", async () => {
-    //
-    //             const { uploadCsvButton } = renderRepertoirePage()
-    //
-    //             userEvent.click(uploadCsvButton)
-    //             const submitButton = screen.getByRole("button", { name: /submit/i})
-    //             const uploadArea = screen.getByLabelText(/click to upload csv/i) as HTMLInputElement
-    //
-    //             let blob : IBlob = new Blob([""], { type: 'text/csv' });
-    //             blob["lastModifiedDate"] = "";
-    //             blob["name"] = "test.csv";
-    //
-    //             let csvFile = blob as File;
-    //
-    //             mockAxios.post.mockResolvedValue({})
-    //             userEvent.click(uploadCsvButton)
-    //
-    //
-    //             userEvent.upload(uploadArea, csvFile)
-    //
-    //             if(uploadArea?.files) {
-    //                 expect(uploadArea.files[0]).toStrictEqual(csvFile)
-    //             }
-    //
-    //             expect(screen.getByText("test.csv")).toBeInTheDocument()
-    //
-    //             userEvent.click(submitButton)
-    //
-    //             expect(await screen.findByText(successMessage)).toBeInTheDocument()
-    //             expect(screen.queryByText("test.csv")).not.toBeInTheDocument()
-    //
-    //             const closeIcon = screen.getByText( /close/i)
-    //             userEvent.click(closeIcon)
-    //             userEvent.click(uploadCsvButton)
-    //
-    //             expect(screen.queryByText("test.csv")).not.toBeInTheDocument()
-    //
-    //
-    //
-    //         })
-    //
-    //         it("should remove files and messages if modal is close without submitting", async () => {
-    //
-    //             const { uploadCsvButton } = renderRepertoirePage()
-    //
-    //
-    //             userEvent.click(uploadCsvButton)
-    //
-    //             const uploadArea = screen.getByLabelText(/click to upload csv/i) as HTMLInputElement
-    //
-    //             let blob : IBlob = new Blob([""], { type: 'text/csv' });
-    //             blob["lastModifiedDate"] = "";
-    //             blob["name"] = "test.csv";
-    //
-    //             let csvFile = blob as File;
-    //
-    //             mockAxios.post.mockResolvedValue({})
-    //             userEvent.click(uploadCsvButton)
-    //
-    //             userEvent.upload(uploadArea, csvFile)
-    //
-    //             if(uploadArea?.files) {
-    //                 expect(uploadArea.files[0]).toStrictEqual(csvFile)
-    //             }
-    //
-    //             const closeIcon = screen.getByText( /close/i)
-    //             userEvent.click(closeIcon)
-    //             userEvent.click(uploadCsvButton)
-    //
-    //             expect(screen.queryByText("test.csv")).not.toBeInTheDocument()
-    //             expect(screen.queryByText(errorMessage)).not.toBeInTheDocument()
-    //             expect(screen.queryByText(successMessage)).not.toBeInTheDocument()
-    //
-    //             const submitButton = screen.getByRole("button", { name: /submit/i})
-    //             userEvent.click(submitButton)
-    //
-    //             expect(screen.getByText(errorMessage)).toBeInTheDocument()
-    //
-    //             jest.clearAllMocks()
-    //
-    //         })
-    //
-    //         it("should show uploading not successful error if the API call fails", async () => {
-    //             mockAxios.post.mockRejectedValue({
-    //                 response: {
-    //                     error: {
-    //                         data: {}
-    //                     }
-    //                 }
-    //             })
-    //             const { uploadCsvButton } = renderRepertoirePage()
-    //
-    //
-    //             userEvent.click(uploadCsvButton)
-    //
-    //             const submitButton = screen.getByRole("button", { name: /submit/i})
-    //             const uploadArea = screen.getByLabelText(/click to upload csv/i) as HTMLInputElement
-    //
-    //             let blob : IBlob = new Blob([""], { type: 'text/csv' });
-    //             blob["lastModifiedDate"] = "";
-    //             blob["name"] = "test.csv";
-    //
-    //             let csvFile = blob as File;
-    //
-    //             userEvent.upload(uploadArea, csvFile)
-    //
-    //             if(uploadArea?.files) {
-    //                 expect(uploadArea.files[0]).toStrictEqual(csvFile)
-    //             }
-    //
-    //             expect(screen.getByText("test.csv")).toBeInTheDocument()
-    //
-    //
-    //             userEvent.click(submitButton)
-    //
-    //             expect(await screen.findByText(/upload failed. please try again later\./i)).toBeInTheDocument()
-    //
-    //
-    //         })
-    //
-    //         it("should show error message if user uploaded a file larger than 1MB", () => {
-    //
-    //         })
-    //
-    //
-    //     })
-    //
-    //
-    // })
+    describe("The CSV Upload function", () => {
+
+        it("should open and close the Upload CSV modal", () => {
+            const { uploadCsvButton } = renderRepertoirePage()
+            userEvent.click(uploadCsvButton)
+            expect(screen.getByRole("button", { name: /submit/i})).toBeInTheDocument()
+            expect(screen.getByText( /click to upload csv/i)).toBeInTheDocument()
+
+            const closeIcon = screen.getByText( /close/i)
+            userEvent.click(closeIcon)
+            expect(screen.queryByRole("button", { name: /submit/i})).not.toBeInTheDocument()
+            expect(screen.queryByText( /click to upload csv/i)).not.toBeInTheDocument()
+
+        })
+
+        describe("The behaviour of the Upload CSV Modal", () => {
+            const errorMessage = /.*please select a \.csv file before submitting.*/i
+            const successMessage = /csv uploaded successfully.*/i
+
+            interface IBlob extends Blob {
+                lastModifiedDate?: string,
+                name?: string
+            }
+
+            it("should show error message if no file is selected", () => {
+                const { uploadCsvButton } = renderRepertoirePage()
+
+
+                userEvent.click(uploadCsvButton)
+                const submitButton = screen.getByRole("button", { name: /submit/i})
+                const closeIcon = screen.getByText( /close/i)
+
+                expect(screen.queryByText(errorMessage)).not.toBeInTheDocument()
+
+                userEvent.click(submitButton)
+                expect(screen.getByText(errorMessage)).toBeInTheDocument()
+
+                userEvent.click(closeIcon)
+                userEvent.click(uploadCsvButton)
+
+                expect(screen.queryByText(errorMessage)).not.toBeInTheDocument()
+
+            })
+
+            it("should show the filename if file is selected", async () => {
+
+                const { uploadCsvButton } = renderRepertoirePage()
+
+                userEvent.click(uploadCsvButton)
+                const submitButton = screen.getByRole("button", { name: /submit/i})
+                const uploadArea = screen.getByLabelText(/click to upload csv/i) as HTMLInputElement
+
+                let blob : IBlob = new Blob([""], { type: 'text/csv' });
+                blob["lastModifiedDate"] = "";
+                blob["name"] = "test.csv";
+
+                let csvFile = blob as File;
+
+                mockAxios.post.mockResolvedValue({})
+                userEvent.click(uploadCsvButton)
+
+
+                userEvent.upload(uploadArea, csvFile)
+
+                if(uploadArea?.files) {
+                    expect(uploadArea.files[0]).toStrictEqual(csvFile)
+                }
+
+                expect(screen.getByText("test.csv")).toBeInTheDocument()
+
+                userEvent.click(submitButton)
+
+                expect(await screen.findByText(successMessage)).toBeInTheDocument()
+                expect(screen.queryByText("test.csv")).not.toBeInTheDocument()
+
+                const closeIcon = screen.getByText( /close/i)
+                userEvent.click(closeIcon)
+                userEvent.click(uploadCsvButton)
+
+                expect(screen.queryByText("test.csv")).not.toBeInTheDocument()
+
+
+
+            })
+
+            it("should remove files and messages if modal is close without submitting", async () => {
+
+                const { uploadCsvButton } = renderRepertoirePage()
+
+
+                userEvent.click(uploadCsvButton)
+
+                const uploadArea = screen.getByLabelText(/click to upload csv/i) as HTMLInputElement
+
+                let blob : IBlob = new Blob([""], { type: 'text/csv' });
+                blob["lastModifiedDate"] = "";
+                blob["name"] = "test.csv";
+
+                let csvFile = blob as File;
+
+                mockAxios.post.mockResolvedValue({})
+                userEvent.click(uploadCsvButton)
+
+                userEvent.upload(uploadArea, csvFile)
+
+                if(uploadArea?.files) {
+                    expect(uploadArea.files[0]).toStrictEqual(csvFile)
+                }
+
+                const closeIcon = screen.getByText( /close/i)
+                userEvent.click(closeIcon)
+                userEvent.click(uploadCsvButton)
+
+                expect(screen.queryByText("test.csv")).not.toBeInTheDocument()
+                expect(screen.queryByText(errorMessage)).not.toBeInTheDocument()
+                expect(screen.queryByText(successMessage)).not.toBeInTheDocument()
+
+                const submitButton = screen.getByRole("button", { name: /submit/i})
+                userEvent.click(submitButton)
+
+                expect(screen.getByText(errorMessage)).toBeInTheDocument()
+
+                jest.clearAllMocks()
+
+            })
+
+            it("should show uploading not successful error if the API call fails", async () => {
+                mockAxios.post.mockRejectedValue({
+                    response: {
+                        error: {
+                            data: {}
+                        }
+                    }
+                })
+                const { uploadCsvButton } = renderRepertoirePage()
+
+
+                userEvent.click(uploadCsvButton)
+
+                const submitButton = screen.getByRole("button", { name: /submit/i})
+                const uploadArea = screen.getByLabelText(/click to upload csv/i) as HTMLInputElement
+
+                let blob : IBlob = new Blob([""], { type: 'text/csv' });
+                blob["lastModifiedDate"] = "";
+                blob["name"] = "test.csv";
+
+                let csvFile = blob as File;
+
+                userEvent.upload(uploadArea, csvFile)
+
+                if(uploadArea?.files) {
+                    expect(uploadArea.files[0]).toStrictEqual(csvFile)
+                }
+
+                expect(screen.getByText("test.csv")).toBeInTheDocument()
+
+                userEvent.click(submitButton)
+                expect(await screen.findByText(/upload failed. please try again later\./i)).toBeInTheDocument()
+
+
+            })
+
+            it("should show error message if user uploaded a file larger than 1MB", () => {
+
+            })
+        })
+    })
 
     describe("The Delete Selected Song button", () => {
         it("should not be visible if there isn't any checked checkbox", async () => {
@@ -242,17 +236,9 @@ describe("The Repertoire Page", () => {
             renderRepertoirePage()
             expect(screen.queryByRole("button", { name: /delete selected/i })).not.toBeInTheDocument()
 
-
-            // const allCheckboxes = await screen.findAllByRole("checkbox")
-
-
-
             await waitFor(() => {
-                // screen.debug(undefined, 20000)
                 expect(screen.getAllByRole("checkbox")).toHaveLength(6)
             })
-            // expect(axios.get).toBeCalledTimes(1)
-
 
         })
     })
