518137c38a29b787aaf4a7fe17a18fbfeedc8540
Isaac Yong <51271686+IsaacYongTY@users.noreply.github.com>
2022-03-02 03:50:03 +0800

    Refactor metatool form (#91)
    * refactored to util function, update path to absolute
    
    * move types and constants
    
    Co-authored-by: Isaac Yong CloudScore <isaac@thecloudscore.com>

---
+++ components/common/MetaToolForm/constants.ts
@@ -0,0 +1,12 @@
+import { Option } from './types';
+
+export const defaultPinyinSyllableOption = {
+    value: 2,
+    label: '2',
+};
+
+export const defaultPinyinSyllableOptions: Option[] = [
+    { value: 1, label: '1' },
+    { value: 2, label: '2' },
+    { value: 99, label: 'All' },
+];
